9af022d78ef2ed9cefefbcbf555fd998685d84a0
HakimAssadi <inf18056@lehre.dhbw-stuttgart.de>
2020-05-31 16:20:42 +0200

    Grid

--- Config.py
+++ Config.py
@@ -1,7 +1,8 @@
 import os
 import pygame
+import Game
 
-SET_CAPTION = pygame.display.set_caption("Tetris clone")
+
 os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide"
 
 pygame.init()
@@ -12,24 +13,45 @@ WHITE = (255, 255, 255)
 BLUE =  (  0,   0, 255)
 GREEN = (  0, 255,   0)
 RED =   (255,   0,   0)
+BLACK = (  0,   0,   0)
 
 
 # Window settings
-WINDOW_WIDTH = 600
-WINDOW_HEIGTH = 1200
-WINDOW_SURFACE = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGTH), 0, 32)
+window_width = 800
+window_heigth = 700
+game_width = 300
+game_heigth = 600
+block_size = 30
+window_surface = pygame.display.set_mode((window_width, window_heigth), 0, 32)
+pygame.display.set_caption("Tetris clone")
+left_corner_x = (window_width - game_width) // 2
+left_corner_y = window_heigth - game_heigth
+
+window_surface.fill(BLACK)
+title = pygame.font.SysFont('arial', bold=1, size=50).render('TETRIS', 1, RED)
+window_surface.blit(title, (left_corner_x + game_width / 2 - (title.get_width() / 2), 30))
 
 # Blocks
 LINE = pygame.Rect(0, 0, 100, 100)
 
+
+def drawGrid():
+    for x in range(game_width):
+        for y in range(game_heigth):
+            rect = pygame.Rect(x * block_size, y * block_size, block_size, block_size)
+            pygame.draw.rect(window_surface, WHITE, rect, 1)
+
 while True:
+    drawGrid()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
 
-    WINDOW_SURFACE.fill(BLUE)
-    WINDOW_SURFACE.fill(RED, LINE)
+
+
     pygame.display.update()
     clock.tick(20000)
+
+
 
