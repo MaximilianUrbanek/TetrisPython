2d596124e7645fe8cf0343f583069a5381e77f2c
HakimAssadi <inf18056@lehre.dhbw-stuttgart.de>
2020-06-01 14:21:16 +0200

    Fenster borders

--- Tetris.py
+++ Tetris.py
@@ -17,89 +17,88 @@ pic = []
 for n in range(8):
     pic.append(pg.transform.scale(pg.image.load("Pictures/{}.png".format(n)), (block_size, block_size)))
 
-
 pg.init()
 clock = pg.time.Clock()
 window_surface = pg.display.set_mode([window_width, window_heigth])
 pg.display.set_caption("Tetris")
 
 # Shapes, 4 Zahlen pro Zeile um Formen zu definieren
-shape_c = [[0,1,1,0,
-           0,1,1,0,
-           0,0,0,0,
-           0,0,0,0]]
-shape_t = [[2,2,2,0,
-           0,2,0,0,
-           0,0,0,0,
-           0,0,0,0],
-           [0,2,0,0,
-            0,2,2,0,
-            0,2,0,0,
-            0,0,0,0],
-           [0,2,0,0,
-            2,2,2,0,
-            0,0,0,0,
-            0,0,0,0],
-           [0,2,0,0,
-            2,2,0,0,
-            0,2,0,0,
-            0,0,0,0]]
-shape_s = [[0,3,3,0,
-            3,3,0,0,
-            0,0,0,0,
-            0,0,0,0],
-           [0,3,0,0,
-            0,3,3,0,
-            0,0,3,0,
-            0,0,0,0]]
-shape_z = [[0,4,4,0,
-            0,0,4,4,
-            0,0,0,0,
-            0,0,0,0],
-           [0,0,4,0,
-            0,4,4,0,
-            0,4,0,0,
-            0,0,0,0]]
-shape_i = [[5,5,5,5,
-            0,0,0,0,
-            0,0,0,0,
-            0,0,0,0],
-           [0,5,0,0,
-            0,5,0,0,
-            0,5,0,0,
-            0,5,0,0]]
-shape_l2 = [[0,6,0,0,
-           0,6,0,0,
-           0,6,6,0,
-           0,0,0,0],
-           [0,0,6,0,
-            6,6,6,0,
-            0,0,0,0,
-            0,0,0,0],
-           [0,6,6,0,
-            0,0,6,0,
-            0,0,6,0,
-            0,0,0,0],
-           [6,6,6,0,
-            6,0,0,0,
-            0,0,0,0,
-            0,0,0,0]]
-shape_l1 = [[0,0,7,0,
-             0,0,7,0,
-             0,7,7,0,
-             0,0,0,0],
-            [7,7,7,0,
-             0,0,7,0,
-             0,0,0,0,
-             0,0,0,0],
-            [0,7,7,0,
-             0,7,0,0,
-             0,7,0,0,
-             0,0,0,0],
-            [0,7,0,0,
-             0,7,7,7,
-             0,0,0,0,
-             0,0,0,0]]
+shape_c = [[0, 1, 1, 0,
+            0, 1, 1, 0,
+            0, 0, 0, 0,
+            0, 0, 0, 0]]
+shape_t = [[2, 2, 2, 0,
+            0, 2, 0, 0,
+            0, 0, 0, 0,
+            0, 0, 0, 0],
+           [0, 2, 0, 0,
+            0, 2, 2, 0,
+            0, 2, 0, 0,
+            0, 0, 0, 0],
+           [0, 2, 0, 0,
+            2, 2, 2, 0,
+            0, 0, 0, 0,
+            0, 0, 0, 0],
+           [0, 2, 0, 0,
+            2, 2, 0, 0,
+            0, 2, 0, 0,
+            0, 0, 0, 0]]
+shape_s = [[0, 3, 3, 0,
+            3, 3, 0, 0,
+            0, 0, 0, 0,
+            0, 0, 0, 0],
+           [0, 3, 0, 0,
+            0, 3, 3, 0,
+            0, 0, 3, 0,
+            0, 0, 0, 0]]
+shape_z = [[0, 4, 4, 0,
+            0, 0, 4, 4,
+            0, 0, 0, 0,
+            0, 0, 0, 0],
+           [0, 0, 4, 0,
+            0, 4, 4, 0,
+            0, 4, 0, 0,
+            0, 0, 0, 0]]
+shape_i = [[5, 5, 5, 5,
+            0, 0, 0, 0,
+            0, 0, 0, 0,
+            0, 0, 0, 0],
+           [0, 5, 0, 0,
+            0, 5, 0, 0,
+            0, 5, 0, 0,
+            0, 5, 0, 0]]
+shape_l2 = [[0, 6, 0, 0,
+             0, 6, 0, 0,
+             0, 6, 6, 0,
+             0, 0, 0, 0],
+            [0, 0, 6, 0,
+             6, 6, 6, 0,
+             0, 0, 0, 0,
+             0, 0, 0, 0],
+            [0, 6, 6, 0,
+             0, 0, 6, 0,
+             0, 0, 6, 0,
+             0, 0, 0, 0],
+            [6, 6, 6, 0,
+             6, 0, 0, 0,
+             0, 0, 0, 0,
+             0, 0, 0, 0]]
+shape_l1 = [[0, 0, 7, 0,
+             0, 0, 7, 0,
+             0, 7, 7, 0,
+             0, 0, 0, 0],
+            [7, 7, 7, 0,
+             0, 0, 7, 0,
+             0, 0, 0, 0,
+             0, 0, 0, 0],
+            [0, 7, 7, 0,
+             0, 7, 0, 0,
+             0, 7, 0, 0,
+             0, 0, 0, 0],
+            [0, 7, 0, 0,
+             0, 7, 7, 7,
+             0, 0, 0, 0,
+             0, 0, 0, 0]]
 shape_list = [shape_c, shape_i, shape_s, shape_t, shape_z, shape_l1, shape_l2]
 
 @dataclass
@@ -113,11 +112,13 @@ class shapes():
             if m > 0:
                 y = (self.start_x + n // 4) * block_size
                 x = (self.start_y + n % 4) * block_size
-                window_surface.blit(pic[m], (x,y))
+                window_surface.blit(pic[m], (x, y))
 
     def update(self, new_x, new_y):
-        self.start_x += new_x
-        self.start_y += new_y
+        if self.valid(self.start_x+new_x, self.start_y+new_y):
+            self.start_x += new_x
+            self.start_y += new_y
+
 
     @staticmethod
     def create():
@@ -128,23 +129,28 @@ class shapes():
             m = 0
         return shapes(shape_list[n][m])
 
-    def change(n, m):
-        if len(shape_list[n]) >  m+1:
-            return shapes(shape_list[n][m+1])
-        else:
-            return shapes(shape_list[n][0])
-
     @staticmethod
     def rotate(shape):
         for n in range(len(shape_list)):
             for m in range(len(shape_list[n])):
                 if shape_list[n][m] == shape:
-                    print(n, m)
-                    return shapes.change(n, m)
+                    if len(shape_list[n]) > m + 1:
+                        return shapes(shape_list[n][m + 1])
+                    else:
+                        return shapes(shape_list[n][0])
+
+    def valid(self, x, y):
+        for n, m in enumerate(self.shape):
+            if m > 0:
+                x1 = x + n // 4
+                y1 = y + n % 4
+                if x1 >= cols or y1 < 0 or y1 >= rows or grid[x1 * rows + y1] > 0:
+                    return False
+        return True
 
 
 # Events
-shape_down = pg.USEREVENT+1
+shape_down = pg.USEREVENT + 1
 pg.time.set_timer(shape_down, 500)
 
 figure = shapes.create()
@@ -156,25 +162,23 @@ while True:
             pg.quit()
             quit()
         if event.type == shape_down:
-            figure.update(1,0)
+            figure.update(1, 0)
         if event.type == pg.KEYDOWN:
             if event.key == pg.K_LEFT:
-                figure.update(0,-1)
+                figure.update(0, -1)
             if event.key == pg.K_RIGHT:
-                figure.update(0,1)
+                figure.update(0, 1)
             if event.key == pg.K_DOWN:
-                figure.update(1,0)
+                figure.update(1, 0)
             if event.key == pg.K_UP:
                 figure = shapes.rotate(figure.shape)
 
-
-
-    window_surface.fill((0,0,0))
+    window_surface.fill((0, 0, 0))
     figure.show()
 
     for n, m in enumerate(grid):
         if m > 0:
             x = n % rows * block_size
             y = n // rows * block_size
-            window_surface.blit(pic[m], (x,y))
+            window_surface.blit(pic[m], (x, y))
     pg.display.flip()
